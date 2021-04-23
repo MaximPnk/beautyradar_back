96fdd72f360b42faa2fa6eac3ec461e57f5c6c3d
Maxim <adm.mjkee@yandex.ru>
2021-04-24 01:56:26 +0300

    init project

---
+++ src/test/java/ru/beautyradar/frontgateway/FrontGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.beautyradar.frontgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
