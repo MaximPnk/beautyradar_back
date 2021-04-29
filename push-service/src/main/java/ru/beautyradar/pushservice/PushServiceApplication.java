816117b24b3c40aed5d298e4ca48aee7e7fb5929
Maxim <adm.mjkee@yandex.ru>
2021-04-30 00:26:40 +0300

    upd properties

--- push-service/src/main/java/ru/beautyradar/pushservice/PushServiceApplication.java
+++ push-service/src/main/java/ru/beautyradar/pushservice/PushServiceApplication.java
@@ -2,8 +2,10 @@ package ru.beautyradar.pushservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "classpath:push-service-secret.properties")
 public class PushServiceApplication {
 
     public static void main(String[] args) {
