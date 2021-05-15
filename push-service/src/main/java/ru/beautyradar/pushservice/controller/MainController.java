eb7600681c7a021c76d427a3ee4225615ae7c80a
Maxim <adm.mjkee@yandex.ru>
2021-05-15 21:31:05 +0300

    change main pages

--- push-service/src/main/java/ru/beautyradar/pushservice/controller/MainController.java
+++ push-service/src/main/java/ru/beautyradar/pushservice/controller/MainController.java
@@ -1,5 +1,7 @@
 package ru.beautyradar.pushservice.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
 
     @GetMapping
-    public void main() {}
+    public ResponseEntity<?> main() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
 }
